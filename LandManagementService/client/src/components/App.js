8fb7894cd90a27a950b009092fe71982f5d0c564
Aarohi Prasad <aarohi.prasad@acquia.com>
2021-07-30 03:03:26 +0530

    Details page complete

--- LandManagementService/client/src/components/App.js
+++ LandManagementService/client/src/components/App.js
@@ -8,6 +8,7 @@ import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer"
 import UploadLandPage from './views/UploadLandPage/UploadLandPage';
+import DetailProductPage from './views/DetailProductPage/DetailProductPage';
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -23,6 +24,7 @@ function App() {
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
           <Route exact path="/product/upload" component={Auth(UploadLandPage, true)} />
+          <Route exact path="/product/:productId" component={Auth(DetailProductPage, null)} />
         </Switch>
       </div>
       <Footer />
---
+++ LandManagementService/client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import { Button, Descriptions } from 'antd';
+
+function ProductInfo(props) {
+
+    const [Product, setProduct] = useState({})
+
+    useEffect(() => {
+
+        setProduct(props.detail)
+
+    }, [props.detail])
+
+    // const addToCarthandler = () => {
+    //     props.addToCart(props.detail._id)
+    // }
+
+
+    return (
+        <div>
+            <Descriptions title="Product Info">
+                <Descriptions.Item label="Price (Rs)"> {Product.price}</Descriptions.Item>
+                <Descriptions.Item label="Area (Sq Km)">{Product.area}</Descriptions.Item>
+                <Descriptions.Item label="Region"> {Product.region}</Descriptions.Item>
+                <Descriptions.Item label="Description"> {Product.description}</Descriptions.Item>
+            </Descriptions>
+
+            <br />
+            <br />
+            <br />
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                
+            </div>
+        </div>
+    )
+}
+
+export default ProductInfo
