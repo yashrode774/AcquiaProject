7f9e769492edc9f35209334013915efe9d5b869f
Aarohi Prasad <aarohi.prasad@acquia.com>
2021-07-30 01:14:10 +0530

    Incomplete UI for landing page

--- LandManagementService/server/routes/product.js
+++ LandManagementService/server/routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
 const { Product } = require("../models/Product");
+const multer = require('multer');
 
 const { auth } = require("../middleware/auth");
 
@@ -25,18 +25,20 @@ var upload = multer({ storage: storage }).single("file")
 
 
 //=================================
-//             Land
+//             Product
 //=================================
 
 router.post("/uploadImage", auth, (req, res) => {
-    //After getting image from client we need to save it inside node server--need multer lib
+
     upload(req, res, err => {
         if (err) {
             return res.json({ success: false, err })
         }
         return res.json({ success: true, image: res.req.file.path, fileName: res.req.file.filename })
     })
+
 });
+
 
 router.post("/uploadProduct", auth, (req, res) => {
 
@@ -49,5 +51,88 @@ router.post("/uploadProduct", auth, (req, res) => {
     })
 
 });
+
+
+router.post("/getProducts", (req, res) => {
+
+    let order = req.body.order ? req.body.order : "desc";
+    let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    let skip = parseInt(req.body.skip);
+
+    let findArgs = {};
+    let term = req.body.searchTerm;
+
+    for (let key in req.body.filters) {
+
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1]
+                }
+            } else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    }
+
+    console.log(findArgs)
+
+    if (term) {
+        Product.find(findArgs)
+            .find({ $text: { $search: term } })
+            .populate("writer")
+            .sort([[sortBy, order]])
+            .skip(skip)
+            .limit(limit)
+            .exec((err, products) => {
+                if (err) return res.status(400).json({ success: false, err })
+                res.status(200).json({ success: true, products, postSize: products.length })
+            })
+    } else {
+        Product.find(findArgs)
+            .populate("writer")
+            .sort([[sortBy, order]])
+            .skip(skip)
+            .limit(limit)
+            .exec((err, products) => {
+                if (err) return res.status(400).json({ success: false, err })
+                res.status(200).json({ success: true, products, postSize: products.length })
+            })
+    }
+
+});
+
+
+//?id=${productId}&type=single
+//id=12121212,121212,1212121   type=array 
+router.get("/products_by_id", (req, res) => {
+    let type = req.query.type
+    let productIds = req.query.id
+
+    console.log("req.query.id", req.query.id)
+
+    if (type === "array") {
+        let ids = req.query.id.split(',');
+        productIds = [];
+        productIds = ids.map(item => {
+            return item
+        })
+    }
+
+    console.log("productIds", productIds)
+
+
+    //we need to find the product information that belong to product Id 
+    Product.find({ '_id': { $in: productIds } })
+        .populate('writer')
+        .exec((err, product) => {
+            if (err) return res.status(400).send(err)
+            return res.status(200).send(product)
+        })
+});
+
+
 
 module.exports = router;
